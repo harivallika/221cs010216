d47b20a34449b2a20bf4b6c081ffb8735441eec1
harivallika <157470809+harivallika@users.noreply.github.com>
2024-01-30 20:03:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import WelcomeMessage from "./components/welcome";
+import StudentInfo from "./components/student-info";
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome,World!</h1>
+     <p>Name:Harivallika</p>
+     <p>Roll No:2211cs010216</p>
+     <p>CGPA:9.00</p>
+     <h1>SKILLS</h1>
+     <op>1.good at listening</op>
+     <p>2.good at python programming</p>
+    </div>
+  );
+}
+
+export default App;
